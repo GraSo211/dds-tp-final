964bff7373a83811cf16c074a17d2f9bff6386ad
Johanna <51618@sistemas.frc.utn.edu.ar>
2023-06-25 18:51:46 -0300

    Se agregan las funcionalidades para Cliente

--- front/src/App.js
+++ front/src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import {Menu} from './components/Menu';
 import { Inicio } from './components/Inicio';
 
-
+import Clientes from './components/clientes/Clientes'
 
 import Animales from './components/animales/Animales'
 
 import Consultas from './components/consultas/Consultas'
 
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
-import ListadoVeterinarios from './components/veterinario/ListadoVeterinarios';
+
 import Veterinarios from './components/veterinario/Veterinarios';
 
 function App() {
@@ -23,7 +23,7 @@ function App() {
               <Route path="/inicio" element={<Inicio />} />
               <Route path="/veterinario" element={<Veterinarios />} />
               <Route path="/animales" element={<Animales />} />
-
+              <Route path="/clientes" element={<Clientes />} />
               <Route path="/consultas" element={<Consultas />} />
 
               <Route path="*" element={<Navigate to="/inicio" replace />} />
